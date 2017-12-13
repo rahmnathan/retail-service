5e1d0818e1fbd886573f551818642c6a868f9bba
nathan <rahm.nathan@gmail.com>
2017-12-12 22:47:45 -0600

    Refactoring

--- retail-service-price-data/src/main/java/com/github/rahmnathan/retail/price/data/boundary/ProductPriceService.java
+++ retail-service-price-data/src/main/java/com/github/rahmnathan/retail/price/data/boundary/ProductPriceService.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Component
 public class ProductPriceService {
+    private final Logger logger = Logger.getLogger(ProductPriceService.class.getName());
     private final ProductPriceRepository productPriceRepository;
 
     @Inject
@@ -19,11 +21,20 @@ public class ProductPriceService {
     public void upsertProductPrice(ProductPrice productPrice){
         validateParams(productPrice);
 
+        if(getProductPrice(productPrice.getId()).isPresent()){
+            logger.info("ProductPrice with id: " + productPrice.getId() + " already exists. Deleting it before insert");
+            productPriceRepository.delete(productPrice.getId());
+        }
+
+        logger.info("Inserting ProductPrice: " + productPrice.toString());
         productPriceRepository.insert(productPrice);
     }
 
-    public Optional<ProductPrice> getProductPrice(Long Id){
-        return Optional.ofNullable(productPriceRepository.findOne(Id));
+    public Optional<ProductPrice> getProductPrice(Long id){
+        ProductPrice productPrice = productPriceRepository.findOne(id);
+
+        logger.info("Query for ProductPrice: " + id + " returned: " + productPrice);
+        return Optional.ofNullable(productPrice);
     }
 
     private void validateParams(ProductPrice productPrice) {
