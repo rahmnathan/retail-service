85b4be3fc25c193289c83bbddc788dc4038e2139
nathan <rahm.nathan@gmail.com>
2017-12-14 19:35:57 -0600

    Removed unused method

--- retail-service-redsky-data-api/src/main/java/com/github/rahmnathan/retail/redsky/data/exception/RedSkyServiceException.java
+++ retail-service-redsky-data-api/src/main/java/com/github/rahmnathan/retail/redsky/data/exception/RedSkyServiceException.java
@@ -5,8 +5,4 @@ public class RedSkyServiceException extends Exception {
     public RedSkyServiceException(String message){
         super(message);
     }
-
-    public RedSkyServiceException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
 }
